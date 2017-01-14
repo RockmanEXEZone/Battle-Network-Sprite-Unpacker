7397f7884de80d5ddbf64f7ba77ffaa98e57702a
Mgamerz <mgamerz2@gmail.com>
2017-01-14 14:15:31 -0700

    Add reading of tilesets. Code needs to be linked and looped, but we need to know where palettes start.

---
+++ BNSA Unpacker/classes/Tileset.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BNSA_Unpacker.classes
+{
+    class Tileset
+    {
+        public long Pointer;
+        public int BitmapSize;
+        public byte[] BitmapData;
+        
+        public Tileset(FileStream stream)
+        {
+            Pointer = stream.Position;
+            BitmapSize = BNSAFile.ReadIntegerFromStream(stream);
+            BitmapData = new byte[BitmapSize];
+            stream.Read(BitmapData, 0, BitmapSize);
+        }
+    }
+}
